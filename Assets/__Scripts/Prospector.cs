f6b26b927e92e2b00219b5d7dd80e7d8aeccc3f8
SimranRiar <simriar@gmail.com>
2018-07-17 20:10:00 -0400

    10 - Game End Feedback

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -28,7 +28,10 @@ public class Prospector : MonoBehaviour
 
     public Vector2 fsPosEnd = new Vector2(0.5f, 0.95f);
 
-    public float reloadDelay = 2f;// 2 sec delay between rounds
+    public float reloadDelay = 2f; // 2 sec delay between rounds
+
+    public Text gameOverText, roundResultText, highScoreText;
+
 
 
 
@@ -53,6 +56,71 @@ public class Prospector : MonoBehaviour
     void Awake()
     {
         S = this;
+        SetUpUITexts();
+
+    }
+
+    void SetUpUITexts()
+    {
+
+        // Set up the HighScore UI Text
+
+        GameObject go = GameObject.Find("HighScore");
+
+        if (go != null)
+        {
+
+            highScoreText = go.GetComponent<Text>();
+
+        }
+
+        int highScore = ScoreManager.HIGH_SCORE;
+
+        string hScore = "High Score: " + Utils.AddCommasToNumber(highScore);
+
+        go.GetComponent<Text>().text = hScore;
+
+
+
+        // Set up the UI Texts that show at the end of the round
+
+        go = GameObject.Find("GameOver");
+
+        if (go != null)
+        {
+
+            gameOverText = go.GetComponent<Text>();
+
+        }
+
+
+
+        go = GameObject.Find("RoundResult");
+
+        if (go != null)
+        {
+
+            roundResultText = go.GetComponent<Text>();
+
+        }
+
+
+
+        // Make the end of round texts invisible
+
+        ShowResultsUI(false);
+
+    }
+
+
+
+    void ShowResultsUI(bool show)
+    {
+
+        gameOverText.gameObject.SetActive(show);
+
+        roundResultText.gameObject.SetActive(show);
+
     }
 
     void Start()
@@ -497,11 +565,21 @@ public class Prospector : MonoBehaviour
         GameOver(false);
     }
 
+
+
     void GameOver(bool won)
     {
+        int score = ScoreManager.SCORE;
+
+        if (fsRun != null) score += fsRun.score;
 
         if (won)
         {
+            gameOverText.text = "Round Over";
+
+            roundResultText.text = "You won this round!\nRound Score: " + score;
+
+            ShowResultsUI(true);
 
             //print("Game Over. You won! :)");
 
@@ -512,6 +590,24 @@ public class Prospector : MonoBehaviour
         }
         else
         {
+            gameOverText.text = "Game Over";
+            if (ScoreManager.HIGH_SCORE <= score)
+            {
+
+                string str = "You got the high score!\nHigh score: " + score;
+
+                roundResultText.text = str;
+
+            }
+            else
+            {
+
+                roundResultText.text = "Your final score was: " + score;
+
+            }
+
+            ShowResultsUI(true);
+
 
             //print("Game Over. You Lost. :(");
 
